0da96364a282403b664b8e484ab1bab589690eaa
xianglong bai <756129421@qq.com>
2023-06-04 15:05:12 +0800

    -----88888----

---
+++ PresiMex/PresiMex/classes/Means/Cer/C/PMCertificationCoreViewController.h
@@ -0,0 +1,16 @@
+//
+//  PMCertificationCoreViewController.h
+//  PresiMex
+//
+//  Created by 白翔龙 on 2023/6/4.
+//
+
+#import "WFBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PMCertificationCoreViewController : WFBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PresiMex/PresiMex/classes/Means/Cer/C/PMCertificationCoreViewController.m
@@ -0,0 +1,31 @@
+//
+//  PMCertificationCoreViewController.m
+//  PresiMex
+//
+//  Created by 白翔龙 on 2023/6/4.
+//
+
+#import "PMCertificationCoreViewController.h"
+
+@interface PMCertificationCoreViewController ()
+
+@end
+
+@implementation PMCertificationCoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
