0a6f8800ccc7cd5a0e681f75292e96cef037e370
xianglong bai <756129421@qq.com>
2023-06-04 13:37:43 +0800

    ---bbb---

---
+++ PresiMex/PresiMex/classes/Means/Bank_u/M/PMBankcardModel.h
@@ -0,0 +1,26 @@
+//
+//  PMBankcardModel.h
+//  PresiMex
+//
+//  Created by 白翔龙 on 2023/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PMBankcardModel : NSObject
+
+@property (nonatomic, copy) NSString *number;
+
+@property (nonatomic, copy) NSString *is_card;
+
+@property (nonatomic, copy) NSString *name;
+
+@property (nonatomic, copy) NSString *bank_Id;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PresiMex/PresiMex/classes/Means/Bank_u/V/PMBankSelectViewCell.m
@@ -0,0 +1,106 @@
+//
+//  PMBankSelectViewCell.m
+//  PresiMex
+//
+//  Created by 白翔龙 on 2023/6/4.
+//
+
+#import "PMBankSelectViewCell.h"
+
+
+
+@interface PMBankSelectViewCell ()
+
+
+@property (nonatomic, strong) UIButton *priaBtn;
+@property (nonatomic, strong) UIButton *wanitaBtn;
+@property (nonatomic, strong) UILabel  *titleLabel;
+@end
+@implementation PMBankSelectViewCell
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.contentView.backgroundColor = [UIColor whiteColor];
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        [self setupSubViews];
+    }
+    return self;
+}
++ (instancetype)cellWithTableView:(UITableView *)tableView
+{
+    static NSString *ID = @"PMBankSelectViewCell";
+    
+    PMBankSelectViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[PMBankSelectViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    return cell;
+}
+-(void)setupSubViews{
+    
+    
+    _titleLabel = [[UILabel alloc] init];
+    [self.contentView addSubview:_titleLabel];
+    _titleLabel.font =B_FONT_REGULAR(12);
+    _titleLabel.textColor =BColor_Hex(@"#1B1200",1);
+    _titleLabel.text=@"Relación";
+    _titleLabel.frame=CGRectMake(15, 20,50, 20);
+    _titleLabel.textAlignment=NSTextAlignmentLeft;
+    
+    UIButton*priaBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    priaBtn.tag=100;
+    priaBtn.frame = CGRectMake(WF_ScreenWidth-80-15-20-150,15,150, 30);
+    [priaBtn setTitle:@"Tarjeta de débito" forState:UIControlStateNormal];
+    [priaBtn setTitleColor:BColor_Hex(@"#1B1200", 1) forState:UIControlStateNormal];
+    priaBtn.titleLabel.font=B_FONT_BOLD(15);
+    [priaBtn addTarget:self action:@selector(clickBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:priaBtn];
+    [priaBtn setImage:[UIImage imageNamed:@"select_normal"] forState:UIControlStateNormal];
+    [priaBtn setImage:[UIImage imageNamed:@"select_sel"] forState:UIControlStateSelected];
+    [priaBtn layoutButtonWithEdgeInsetsStyle:MKButtonEdgeInsetsStyleLeft imageTitleSpace:10];
+    priaBtn.selected=NO;
+    _priaBtn=priaBtn;
+    
+    
+    UIButton*wanitaBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    wanitaBtn.tag=101;
+    wanitaBtn.frame = CGRectMake(WF_ScreenWidth-80-15, 15,80, 30);
+    [wanitaBtn setTitle:@"CLABE" forState:UIControlStateNormal];
+    [wanitaBtn setTitleColor:BColor_Hex(@"#1B1200", 1) forState:UIControlStateNormal];
+    wanitaBtn.titleLabel.font=B_FONT_BOLD(15);
+    [wanitaBtn addTarget:self action:@selector(clickBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:wanitaBtn];
+    [wanitaBtn setImage:[UIImage imageNamed:@"select_normal"] forState:UIControlStateNormal];
+    [wanitaBtn setImage:[UIImage imageNamed:@"select_sel"] forState:UIControlStateSelected];
+    [wanitaBtn layoutButtonWithEdgeInsetsStyle:MKButtonEdgeInsetsStyleLeft imageTitleSpace:10];
+    wanitaBtn.selected=NO;
+    _wanitaBtn=wanitaBtn;
+    
+}
+
+-(void)clickBtn:(UIButton*)sender{
+    
+    sender.selected=!sender.selected;
+    
+    if (sender.tag==100 && sender.selected) {
+        _wanitaBtn.selected=!sender.selected;
+    }
+    if (sender.tag==101 && sender.selected) {
+        _priaBtn.selected=!sender.selected;
+    }
+    if (self.wanitaBtn.selected && self.click) {
+        self.click(sender.tag);
+    }
+    if (self.priaBtn.selected && self.click) {
+        self.click(sender.tag);
+    }
+}
+
+-(void)setCellWithModel:(PMBankcardModel*)model{
+    _wanitaBtn.selected=NO;
+    _priaBtn.selected=NO;
+}
+
+
+@end
